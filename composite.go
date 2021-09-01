b127257d0a1bc846e1ad75c934f017b5af35c7a1
Qing Miao <digisanQM@outlook.com>
2021-09-01 11:20:06 +1000

    * composite => (compositeIncl & compositeExcl)

--- composite.go
+++ composite.go
@@ -1,13 +1,27 @@
 package jsontool
 
 import (
+	"github.com/digisan/gotk/slice/ts"
 	"github.com/tidwall/sjson"
 )
 
-func Composite(m map[string]interface{}) string {
+func CompositeExcl(m map[string]interface{}, exclPaths ...string) string {
 	jsonbytes, _ := sjson.SetBytes([]byte(""), "", "") // empty json doc to reinflate with tuples
 	for path, value := range m {
+		if exclPaths != nil && ts.In(path, exclPaths...) {
+			continue
+		}
 		jsonbytes, _ = sjson.SetBytes(jsonbytes, path, value)
+	}
+	return string(jsonbytes)
+}
+
+func CompositeIncl(m map[string]interface{}, inclPaths ...string) string {
+	jsonbytes, _ := sjson.SetBytes([]byte(""), "", "") // empty json doc to reinflate with tuples
+	for path, value := range m {
+		if inclPaths != nil && ts.In(path, inclPaths...) {
+			jsonbytes, _ = sjson.SetBytes(jsonbytes, path, value)
+		}
 	}
 	return string(jsonbytes)
 }
--- composite_test.go
+++ composite_test.go
@@ -13,6 +13,10 @@ func TestComposite(t *testing.T) {
 	}
 	m, err := Flatten(data)
 	fmt.Println(len(m), err)
-	str := Composite(m)
-	os.WriteFile("./data/FlattenTest_Composite.json", []byte(str), os.ModePerm)
+
+	str := CompositeIncl(m, "array.0.c", "object.a", "object1.object11.a")
+	os.WriteFile("./data/FlattenTest_CompositeIncl.json", []byte(str), os.ModePerm)
+
+	str = CompositeExcl(m, "array.0.c", "object.a", "object1.object11.a")
+	os.WriteFile("./data/FlattenTest_CompositeExcl.json", []byte(str), os.ModePerm)
 }
