65f54cfc7d1f0349ddf8895329ddfc1281857d62
Qing Miao <digisanqm@outlook.com>
2024-03-13 15:25:56 +1100

    * validator rewrite

--- validator/validate.go
+++ validator/validate.go
@@ -1,6 +1,8 @@
 package validator
 
 import (
+	"strings"
+
 	. "github.com/digisan/go-generics"
 	dt "github.com/digisan/gotk/data-type"
 	"github.com/tidwall/gjson"
@@ -10,20 +12,14 @@ func IsMissing(r gjson.Result) bool {
 	return r.Type == gjson.Null && len(r.Raw) == 0
 }
 
+var NotExist = IsMissing
+
 func Exists(r gjson.Result) bool {
 	return !IsMissing(r)
 }
 
 func IsNull(r gjson.Result) bool {
 	return r.Type == gjson.Null && r.Raw == "null"
-}
-
-func HasValue(r gjson.Result) bool {
-	return Exists(r) && !IsNull(r)
-}
-
-func HasSomeValue(r gjson.Result) bool {
-	return HasValue(r) && !IsEmptyStr(r) && !IsEmptyArr(r) && !IsNestedEmptyArr(r) && !IsAllNullElemArr(r) && !IsAllEmptyStrElemArr(r)
 }
 
 func IsBool(r gjson.Result) bool {
@@ -34,18 +30,74 @@ func IsNum(r gjson.Result) bool {
 	return r.Type == gjson.Number
 }
 
-func IsObj(r gjson.Result) bool {
-	return r.IsObject()
-}
-
-////////////////////////
-
 func IsStr(r gjson.Result) bool {
 	return r.Type == gjson.String
 }
 
+func IsObj(r gjson.Result) bool {
+	return r.IsObject()
+}
+
+func IsArr(r gjson.Result) bool {
+	return r.IsArray()
+}
+
 func IsEmptyStr(r gjson.Result) bool {
 	return IsStr(r) && len(r.Str) == 0
+}
+
+func IsEmptyObj(r gjson.Result) bool {
+	check := Filter([]byte(r.Raw), func(i int, e byte) bool {
+		return NotIn(e, ' ', '\t', '\n')
+	})
+	return IsObj(r) && string(check) == "{}"
+}
+
+func IsEmptyArr(r gjson.Result) bool {
+	// return r.IsArray() && r.Raw == "[]"
+	return r.IsArray() && len(r.Array()) == 0
+}
+
+////////////////////////
+
+func HasNotNullValue(r gjson.Result) bool {
+	return Exists(r) && !IsNull(r)
+}
+
+func HasSomeValue(r gjson.Result) bool {
+	if NotExist(r) {
+		return false
+	}
+	if IsNull(r) {
+		return false
+	}
+	if IsEmptyStr(r) {
+		return false
+	}
+	if IsEmptyObj(r) {
+		return false
+	}
+	if IsEmptyArr(r) {
+		return false
+	}
+	return true
+}
+
+func HasMeaningfulValue(r gjson.Result) bool {
+	if HasSomeValue(r) {
+		return !IsBlankStr(r)
+	}
+	return false
+}
+
+func HasEmptyValue(r gjson.Result) bool {
+	return HasNotNullValue(r) && !HasSomeValue(r)
+}
+
+////////////////////////
+
+func IsBlankStr(r gjson.Result) bool {
+	return IsStr(r) && !IsEmptyStr(r) && len(strings.TrimSpace(r.Str)) == 0
 }
 
 func IsHTMLStr(r gjson.Result) bool {
@@ -58,166 +110,136 @@ func IsPlainStr(r gjson.Result) bool {
 
 ////////////////////////
 
-func IsArr(r gjson.Result) bool {
-	return r.IsArray()
-}
-
-func IsNestedArr(r gjson.Result) bool {
+func IsXArray(r gjson.Result, f func(e gjson.Result) bool) bool {
 	if IsArr(r) {
 		for _, e := range r.Array() {
-			if !IsArr(e) {
+			if !f(e) {
 				return false
 			}
 		}
-		return true
+		return len(r.Array()) > 0
 	}
 	return false
 }
 
-func IsEmptyArr(r gjson.Result) bool {
-	return r.IsArray() && len(r.Array()) == 0
+func IsNullElemArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsNull(e)
+	})
 }
 
-func IsAllNullElemArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsNull(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsEmptyElemArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return HasEmptyValue(e)
+	})
 }
 
-func IsAllEmptyStrElemArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsEmptyStr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsStrArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsStr(e)
+	})
 }
 
-func IsNestedEmptyArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsEmptyArr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsPlainStrArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsPlainStr(e)
+	})
 }
 
-func IsStrArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsStr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsHTMLStrArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsHTMLStr(e)
+	})
 }
 
-func IsNestedStrArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsStrArr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsURLStrArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsURL(e.Str)
+	})
 }
 
-func IsPlainStrArr(r gjson.Result) bool {
-	if IsStrArr(r) {
-		for _, e := range r.Array() {
-			if !IsPlainStr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsObjArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsObj(e)
+	})
 }
 
-func IsNestedPlainStrArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsPlainStrArr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
+func IsNestedArray(r gjson.Result) bool {
+	return IsXArray(r, func(e gjson.Result) bool {
+		return IsArr(e)
+	})
 }
 
-func IsHTMLStrArr(r gjson.Result) bool {
-	if IsStrArr(r) {
-		for _, e := range r.Array() {
-			if !IsHTMLStr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
+////////////////////////
 
-func IsNestedHTMLStrArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsHTMLStrArr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
+// func IsAllEmptyStrElemArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsEmptyStr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
 
-func IsURLStrArr(r gjson.Result) bool {
-	if IsStrArr(r) {
-		for _, e := range r.Array() {
-			if !IsURL(e.Str) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
+// func IsNestedEmptyArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsEmptyArr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
 
-func IsObjArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsObj(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
+// func IsNestedStrArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsStrArr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
 
-func IsNestedObjArr(r gjson.Result) bool {
-	if IsArr(r) {
-		for _, e := range r.Array() {
-			if !IsObjArr(e) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
+// func IsNestedPlainStrArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsPlainStrArr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func IsNestedHTMLStrArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsHTMLStrArr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func IsNestedObjArr(r gjson.Result) bool {
+// 	if IsArr(r) {
+// 		for _, e := range r.Array() {
+// 			if !IsObjArr(e) {
+// 				return false
+// 			}
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -2,98 +2,122 @@ package validator
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	dt "github.com/digisan/gotk/data-type"
 	"github.com/tidwall/gjson"
 )
 
-const js = `{
-	"Entity": "<p>abc</p>",
-	"OtherNames": [
-	  "DEF",
-	  "SES"
-	],
-	"Definition": "",
-	"SIF": [
-	  {
-		"XPath": [],
-		"Definition": "abc",
-		"Commentary": "",
-		"Datestamp": ""
-	  },
-	  {
-		"XPath": [],
-		"Definition": "def",
-		"Commentary": "",
-		"Datestamp": ""
-	  }
-	]
-}`
+func report(js string, fields ...string) {
+	for _, field := range fields {
+		fmt.Printf("===========> %v\n", field)
+		r := gjson.Get(js, field)
+		fmt.Printf("\n%s\n\n", r.Raw)
+		if IsMissing(r) {
+			fmt.Printf("'%s' is missing\n", field)
+		}
+		if IsNull(r) {
+			fmt.Printf("'%s' is null\n", field)
+		}
+		if HasNotNullValue(r) {
+			fmt.Printf("'%s' HasNotNullValue\n", field)
+		}
+		if HasEmptyValue(r) {
+			fmt.Printf("'%s' HasEmptyValue\n", field)
+		}
+		if HasSomeValue(r) {
+			fmt.Printf("'%s' HasSomeValue\n", field)
+		}
+		if IsBlankStr(r) {
+			fmt.Printf("'%s' IsBlankStr\n", field)
+		}
+		if IsNullElemArray(r) {
+			fmt.Printf("'%s' IsNullElemArray\n", field)
+		}
+		if IsEmptyElemArray(r) {
+			fmt.Printf("'%s' IsEmptyElemArray\n", field)
+		}
+		fmt.Println()
+	}
+}
 
 func TestTemp(t *testing.T) {
 
-	if !dt.IsJSON([]byte(js)) {
+	data, err := os.ReadFile("./data.json")
+	if err != nil {
+		fmt.Println("reading json file error")
+		return
+	}
+	js := string(data)
+
+	if !dt.IsJSON(data) {
 		fmt.Printf("not valid json")
 		return
 	}
 
-	field := "Entity"
-	r := gjson.Get(js, field)
+	report(
+		js,
+		"Missing",
+		"Null",
+		"EmptyStr",
+		"BlankStr",
+		"EmptyObject",
+		"EmptyArray",
+		"NullElemArray",
+		"EmptyStrElemArray",
+		"EmptyArrElemArray",
+		"EmptyObjElemArray",
+	)
 
-	if IsMissing(r) {
-		fmt.Printf("'%s' is missing\n", field)
-		return
-	}
+	// if IsNull(r) {
+	// 	fmt.Printf("'%s' is null\n", field)
+	// }
 
-	if IsNull(r) {
-		fmt.Printf("'%s' is null\n", field)
-	}
+	// if IsEmptyStr(r) {
+	// 	fmt.Printf("'%s' is empty string\n", field)
+	// }
 
-	if IsEmptyStr(r) {
-		fmt.Printf("'%s' is empty string\n", field)
-	}
+	// if IsHTMLStr(r) {
+	// 	fmt.Printf("'%s' is html string\n", field)
+	// }
 
-	if IsHTMLStr(r) {
-		fmt.Printf("'%s' is html string\n", field)
-	}
+	// if IsPlainStr(r) {
+	// 	fmt.Printf("'%s' is plain string\n", field)
+	// }
 
-	if IsPlainStr(r) {
-		fmt.Printf("'%s' is plain string\n", field)
-	}
+	// if IsStr(r) {
+	// 	fmt.Printf("'%s' is string\n", field)
+	// }
 
-	if IsStr(r) {
-		fmt.Printf("'%s' is string\n", field)
-	}
+	// // fmt.Printf("not nil,", r.Num, r.Raw, r.Str, r.Type)
+	// fmt.Println("----------------------------------------")
 
-	// fmt.Printf("not nil,", r.Num, r.Raw, r.Str, r.Type)
-	fmt.Println("----------------------------------------")
+	// field = "OtherNames"
+	// // field = "SIF"
+	// r = gjson.Get(js, field)
 
-	field = "OtherNames"
-	// field = "SIF"
-	r = gjson.Get(js, field)
+	// if IsMissing(r) {
+	// 	fmt.Printf("'%s' is missing\n", field)
+	// 	return
+	// }
 
-	if IsMissing(r) {
-		fmt.Printf("'%s' is missing\n", field)
-		return
-	}
+	// if IsArr(r) {
+	// 	fmt.Printf("'%s' is array\n", field)
+	// }
 
-	if IsArr(r) {
-		fmt.Printf("'%s' is array\n", field)
-	}
+	// if IsEmptyArr(r) {
+	// 	fmt.Printf("'%s' is empty array\n", field)
+	// }
 
-	if IsEmptyArr(r) {
-		fmt.Printf("'%s' is empty array\n", field)
-	}
+	// if IsStrArr(r) {
+	// 	fmt.Printf("'%s' is string array\n", field)
+	// }
 
-	if IsStrArr(r) {
-		fmt.Printf("'%s' is string array\n", field)
-	}
+	// if IsObjArr(r) {
+	// 	fmt.Printf("'%s' is object array\n", field)
+	// }
 
-	if IsObjArr(r) {
-		fmt.Printf("'%s' is object array\n", field)
-	}
-
-	r1 := gjson.Get(js, "SIF.#.Definition")
-	fmt.Printf("SIF.#.Definition: %+v\n", r1.Array())
+	// r1 := gjson.Get(js, "SIF.#.Definition")
+	// fmt.Printf("SIF.#.Definition: %+v\n", r1.Array())
 }
